72c28c9fe240b405c9f329deabbcbb8953aa6ed2
Jan Machek <jan.machek.s@gyarab.cz>
2026-05-19 13:09:20 +0200

    Remove unused URL patterns from urls.py

--- prjmachek/prj/prj/urls.py
+++ prjmachek/prj/prj/urls.py
@@ -2,10 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
-
     path('api-playground/', views.api_playground, name='api_playground'),
-
-
-    path('api/polozky/', views.api_polozky, name='api_polozky'),
+    path('api/books/', views.api_books, name='api_books'),
 ]
