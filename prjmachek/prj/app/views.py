27afa2fae1bff14f623c19a9abb5ee883adeb3ef
Jan Machek <jan.machek.s@gyarab.cz>
2026-05-19 13:08:00 +0200

    Update views.py

--- prjmachek/prj/app/views.py
+++ prjmachek/prj/app/views.py
@@ -1,22 +1,19 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .models import Polozka 
+from .models import Book
 
 
 def api_playground(request):
     return render(request, 'myapp/api_playground.html')
 
 
-def api_polozky(request):
-
+def api_books(request):
     q = request.GET.get('q', '')
 
-
-    qs = Polozka.objects.all()
+    qs = Book.objects.select_related('author').all()
     if q:
-        qs = qs.filter(nazev__icontains=q)
+        qs = qs.filter(title__icontains=q)
 
-
-    data = list(qs.values('id', 'nazev', 'popis'))  # ← uprav pole
+    data = list(qs.values('id', 'title', 'published_date', 'author__name'))
 
     return JsonResponse({'polozky': data, 'pocet': len(data)})
